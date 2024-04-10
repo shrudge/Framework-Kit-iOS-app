9ce2d001dc4106cda2670c5d6d16abca63304c80
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-10 16:05:17 +0530

    Initial Commit

---
+++ Framework-Kit/Framework_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Framework_KitApp.swift
+//  Framework-Kit
+//
+//  Created by Meet Balani on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Framework_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

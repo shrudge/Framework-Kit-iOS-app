7812c9e90d001c7db6e444aa8bc0524218d92793
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-10 17:42:41 +0530

    created details view

---
+++ Framework-Kit/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Framework-Kit
+//
+//  Created by Meet Balani on 10/04/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
